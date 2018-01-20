953b15bfccaed145426e3e67e9226d3b4ede17be
jwang1 <sigkdd@yahoo.com>
2018-01-20 12:08:44 -0500

    Added attribute-directive
    1. need to have brackets in 'selector'
    
    2. also, need to have the selector typed correctly and match what's on Template
       otherwise, it will NOT hit the directive

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import {BasicHighlightDirective} from './shared/basic-highlight.directive';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BasicHighlightDirective
   ],
   imports: [
     BrowserModule
