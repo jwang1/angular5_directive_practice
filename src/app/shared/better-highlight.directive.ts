36f1b0e339429010ab269ae89394b905639f22d3
jwang1 <sigkdd@yahoo.com>
2018-01-20 14:20:49 -0500

    using @HostBinding for attributes to TypeScript property,
    and can set the values in previous @HostListener callbacks.

--- src/app/shared/better-highlight.directive.ts
+++ src/app/shared/better-highlight.directive.ts
@@ -1,9 +1,12 @@
-import {Directive, ElementRef, HostListener, OnInit, Renderer2} from '@angular/core';
+import {Directive, ElementRef, HostBinding, HostListener, OnInit, Renderer2} from '@angular/core';
 
 @Directive({
   selector: '[appBetterHighlight]'
 })
 export class BetterHighlightDirective implements OnInit {
+  // binding attribute for that element applied with this Directie of [appBetterHighlight]
+  @HostBinding('style.color') fontColor = 'black';
+
   constructor(private elemRef: ElementRef, private renderRef: Renderer2) {}
 
   ngOnInit() {
@@ -12,9 +15,14 @@ export class BetterHighlightDirective implements OnInit {
 
   @HostListener('mouseenter') mouseOver() {
     this.renderRef.setStyle(this.elemRef.nativeElement, 'backgroundColor', 'lightgreen');
+
+    this.fontColor = 'red';
   }
 
   @HostListener('mouseleave') mouseLeave() {
     this.renderRef.setStyle(this.elemRef.nativeElement, 'background-color', 'transparent');
+
+    this.fontColor = 'blue';
   }
+
 }
